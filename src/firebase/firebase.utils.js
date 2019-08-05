b05b8e0550928bbfdfbbca10ad4456fb8b428b62
emapop <ematabita14@gmail.com>
2019-08-05 16:17:41 +0300

    implemented firebase utils, included ability to store authentificated users into our firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,8 +12,34 @@ const config = {
   appId: "1:135536884426:web:b90f931589fdb620"
 };
 
-
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+if (!snapShot.exists) {
+  const { displayName, email } = userAuth;
+  const createdAt = new Date();
+
+  try {
+    await userRef.set({
+      displayName,
+      email, 
+      createdAt,
+      ...additionalData
+    });
+
+  } catch (error) {
+  console.log('Error creating user', error.message);
+  }
+}
+
+return userRef;
+} ;
 
 // configuring the firebase utility that we get
 
---
+++ src/test.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('user').doc('Tmtkl2EH0mo9bXhYqqbv').collection('nameItems').doc('aBXZUfFDmfL8LOnInZtv');
+firestore.doc('user/Tmtkl2EH0mo9bXhYqqbv/nameItems/aBXZUfFDmfL8LOnInZtv');
+firestore.collection('user/Tmtkl2EH0mo9bXhYqqbv/nameItems/');
+
+
+
+
