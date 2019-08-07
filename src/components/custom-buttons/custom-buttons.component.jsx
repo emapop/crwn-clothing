4188bcce9d8171c2703be8500bb0463fe8d41157
emapop <ematabita14@gmail.com>
2019-08-07 15:13:03 +0300

    updated shop data with new data, updated cart utils to group things in cart reducer

--- src/components/custom-buttons/custom-buttons.component.jsx
+++ src/components/custom-buttons/custom-buttons.component.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 // ...otherProps for the type submit
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in': ''} custom-button`} {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+    <button className={`${inverted ? 'inverted': ''} custom-button`} {...otherProps}>
         {children}
     </button>
 );
